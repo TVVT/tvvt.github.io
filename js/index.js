dbd27272b71e90d3e760d6fafbd5114329e05e6b
fangcao <836036143.qq.com>
2014-06-24 23:10:59 +0800

    Site updated: 2014-06-24 23:10:56

---
+++ js/index.js
@@ -0,0 +1,31 @@
+var tvvtObj = {
+    tagcloud: function() {
+        var $tagcloud = $(".tagcloud");
+        if ( !! $tagcloud.length) {
+            var $tagcloud_a = $tagcloud.find("a"),
+                $temp;
+            for (var i = 0; i < $tagcloud_a.length; i++) {
+                $temp = $tagcloud_a.eq(i).text().replace(/^s*|s*$/g, '');
+
+                if ($temp === "iOS" || $temp === "Objective-C") {
+                    $tagcloud_a.eq(i).css({
+                        "font-size": "20px"
+                    });
+                }
+            }
+        }
+    },
+    brDelete:function(){
+        var $photoTwo=$(".photoTwo");
+        if(!!$photoTwo.length){
+            $photoTwo.find("br").remove();
+        }
+    },
+    inition: function() {
+        this.tagcloud();
+        this.brDelete();
+    }
+}
+$(document).ready(function() {
+    tvvtObj.inition();
+});
