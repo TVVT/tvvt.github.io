84899b2f284036852ba4d90709d8f4e81eb1fde1
fangcao <836036143.qq.com>
2014-06-27 15:33:08 +0800

    Site updated: 2014-06-27 15:33:08

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var tvvtObj = {
         if(!!$photoTwo.length){
             $photoTwo.find("br").remove();
         }
-        $("footer .alignleft").next().remove();
+        //$("footer .alignleft").next().remove();
     },
     inition: function() {
         this.tagcloud();
